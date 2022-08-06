cea8e660b8f9514a6f6cd5740852ce80037f2d92
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2022-08-06 12:31:46 -0300

    feat: adicionando db.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Navbar />
+        <Container customClass="min-height">
+          <Switch>
+              <Route exact path='/'>
+              <Home />
+              </Route>
+              <Route exact path='/company'>
+                <Company />
+              </Route>
+              <Route exact path='/Projects'>
+                <Projects />
+              </Route>
+              <Route exact path='/contact'>
+                <Contact />
+              </Route>
+              <Route exact path='/newproject'>
+                <NewProject />
+              </Route>
+          </Switch>
+        </Container>
+        <Footer />
+    </Router>
   );
 }
 
