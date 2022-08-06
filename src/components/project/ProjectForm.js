917c14ef8ce39276b3ce79b1d0032f6cba29a7a1
Ruan Noleto <84647446+ruannysil@users.noreply.github.com>
2022-08-06 12:35:42 -0300

    feat: adicionando db.json

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+
+
+function ProjectForm({ handleSubmit, btnText, projectData }) {
+
+    const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+    function handleOnChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+        
+    }
+
+    // function handleCategory(e) {
+    //     setProject({ ...project,  })
+       
+    // }
+
+    return (
+        <form onSubmit={submit} className={styles.form}>
+           <Input 
+                type="text" 
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+                handleOnChange={ handleOnChange }
+           />
+            <Input type="numb"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+                handleOnChange={ handleOnChange }
+                />
+                <Select name="category_id" text="Selecione a categoria" options={categories} />
+                <SubmitButton text={btnText}
+                />
+        </form>
+    )
+}
+
+export default ProjectForm
